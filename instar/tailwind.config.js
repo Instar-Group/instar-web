c558dfea4d0ca2270da82da75be0c56717510324
Archit Kumar <architkumar1712@gmail.com>
2022-06-25 13:00:25 +0530

    rewrote all code with astro

---
+++ instar/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts,astro}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'k2d': ['K2D']
+      }
+    },
+  },
+  plugins: [],
+}
