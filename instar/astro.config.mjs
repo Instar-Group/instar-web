0a5262743b81594ec316a904647581c63368f2c5
Archit Kumar <architkumar1712@gmail.com>
2022-06-25 15:07:51 +0530

    optimized SEO

--- instar/astro.config.mjs
+++ instar/astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import astro from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+	site: 'https://instar.vercel.app',
+	experimental: {
+	  integrations: true,
+	},
+  integrations: [tailwind(), astro()]
 });
