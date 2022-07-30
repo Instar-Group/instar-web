6cb78258d9407196c56d848cf55d630b627d899c
Archit Kumar <architkumar1712@gmail.com>
2022-07-30 20:11:20 +0530

    fix for another error

--- instar/components/TimelineEvent.tsx
+++ instar/components/TimelineEvent.tsx
@@ -4,10 +4,11 @@ import { BsDot } from 'react-icons/bs'
 
 
 export const TimelineEvent = ({content}:{content:newEvent}) => {
+  const eventDate = String(content.createdAt)
   return (
     <div className='flex flex-row items-center space-x-8 '>
         <BsDot size={70}/>
-        <span className='mt4'>{(content.createdAt).substring(0, 10)}</span>
+        <span className='mt4'>{eventDate.substring(0, 10)}</span>
         <span className='mt4'>{content.eventName}</span>
         <span className='mt4'>{content.pointsAlloted}</span>
     </div>
