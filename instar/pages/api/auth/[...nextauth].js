b95b9783ec06ebda542a3a2b895f01896857efcb
Archit Kumar <architkumar1712@gmail.com>
2022-07-30 20:20:39 +0530

    ts to js

--- instar/pages/api/auth/[...nextauth].js
+++ instar/pages/api/auth/[...nextauth].js
@@ -10,7 +10,7 @@ export default NextAuth({
         username: { label: "Username", type: "text" },
         password: { label: "Password", type: "password" },
       },
-      authorize: (credentials:any) => {
+      authorize: (credentials) => {
         if (
           credentials.username === process.env.ADMIN_USERNAME &&
           credentials.password === process.env.ADMIN_PASSWORD
