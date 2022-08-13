4e9f7909e8c60e2fbcab452eab2a905852ee64ff
Archit Kumar <architkumar1712@gmail.com>
2022-08-13 21:05:10 +0530

    responsive??

--- instar/components/InstarLogo.tsx
+++ instar/components/InstarLogo.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const InstarLogo = () => {
   return (
-    <div>
-      <div className="logo">
-        <Image src="/images/InstarLogo.svg" alt="Logo" width={70} height={70} />
-        <span className="logo-text">Instar</span>
-      </div>
+    <div className="cursor-pointer mt-10">
+      <Link href="/">
+        <div className="logo">
+          <Image src="/images/InstarLogo.svg" alt="Logo" width={70} height={70} />
+          <span className="logo-text">Instar</span>
+        </div>
+      </Link>
     </div>
   );
 };
--- instar/pages/gladiators/timeline.tsx
+++ instar/pages/gladiators/timeline.tsx
@@ -1,36 +1,32 @@
 import InstarLogo from "components/InstarLogo";
 import Head from "next/head";
 import Image from "next/image";
-import { BsDot } from "react-icons/bs";
-import { PrismaClient,newEvent } from "@prisma/client";
+import { PrismaClient, newEvent } from "@prisma/client";
 import prisma from "pages/api/prisma";
 import { TimelineEvent } from "components/TimelineEvent";
 
-
-
-export const getServerSideProps = async () =>{
+export const getServerSideProps = async () => {
   const Events: any = await prisma.newEvent.findMany({
-    where:{
-      houseName: "Gladiators"
+    where: {
+      houseName: "Gladiators",
     },
-    take:-12,
-    select:{
-      id:true,
-      houseName:true,
-      eventName:true,
-      pointsAlloted:true,
-      createdAt:true,
-    }
-  })
-  const fetchedEvents:any = JSON.stringify(Events)
+    take: -8,
+    select: {
+      id: true,
+      houseName: true,
+      eventName: true,
+      pointsAlloted: true,
+      createdAt: true,
+    },
+  });
+  const fetchedEvents: any = JSON.stringify(Events);
   return {
-    props: {fetchedEvents}
-  }
-}
+    props: { fetchedEvents },
+  };
+};
 
-
-const timeline = ({fetchedEvents}:{fetchedEvents:any}) => {
-  const latestEvents:any = JSON.parse(fetchedEvents)
+const timeline = ({ fetchedEvents }: { fetchedEvents: any }) => {
+  const latestEvents: any = JSON.parse(fetchedEvents);
   return (
     <div>
       <Head>
@@ -39,35 +35,32 @@ const timeline = ({fetchedEvents}:{fetchedEvents:any}) => {
       </Head>
       <main className="flex items-center flex-col justify-center">
         <InstarLogo />
-        {/* House Logo */}
-        <div className="flex items-start w-full justify-around h-full space-x ">
-            <div className="flex flex-row h-full px-4">
-              <div  className=" mt-28  justify-start border  h-3/4 ">
-              </div>
-                <div className=" text-white text-2xl text-center space-x-8  mt-28 flex flex-row">
-                  <div className="flex flex-col space-y-4">
-                    {latestEvents.reverse().map((event:any, i:number) =>(
-                      <TimelineEvent content={event} key={i} />
-                    ))}
-                  </div>
-                </div>
+        <div className="flex scale-50 md:scale-75 lg:scale-100 items-start w-full justify-around h-full  ">
+          {/* Timeline */}
+          <div className=" text-white text-2xl text-center space-x-8  mt-28 flex flex-row">
+            <div className="flex flex-col space-y-4">
+              {latestEvents.reverse().map((event: any, i: number) => (
+                <TimelineEvent content={event} key={i} />
+              ))}
             </div>
-            <div className="">
-              <div className="logo mt-5 pr-80">
-                <Image
-                  src="/images/Emerald.svg"
-                  alt="Logo"
-                  width={40}
-                  height={40}
-                />
-                <span className="logo-text text-3xl">Gladiators</span>
-              </div>
+          </div>
+          {/* House Logo */}
+          <div>
+            <div className="logo mt-5 pr-80">
+              <Image
+                src="/images/Emerald.svg"
+                alt="Logo"
+                width={40}
+                height={40}
+              />
+              <span className="logo-text text-3xl">Gladiators</span>
             </div>
-            <div className="flex flex-col text-white text-2xl">
-              <span>Captain:xyz</span>
-              <span>Vice Captain:xyz</span>
-            </div>
-            
+          </div>
+          {/* Captain & Vice Captain */}
+          <div className="flex flex-col text-white text-2xl">
+            <span>Captain:xyz</span>
+            <span>Vice Captain:xyz</span>
+          </div>
         </div>
       </main>
     </div>
